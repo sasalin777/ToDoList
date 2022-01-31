8a917ad176da671dd2832fbedce8b762109d43a3
sasalin777 <candysasame@gmail.com>
2022-01-30 22:31:32 -0800

    update

---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Li Tzu Lin on 2022-01-30.
+//
+
+import UIKit
+
+// UITableViewDelegate (action, behaviours)
+
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos = [ToDo]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let savedToDos = ToDo.loadToDos() {
+            toDos = savedToDos
+        } else {
+            toDos = ToDo.loadSampleToDos()
+        }
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+
+       
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = toDo.title
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            toDos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func unwindToToDoList(segue: UIStoryboardSegue) {
+        guard segue.identifier == "saveUnwind" else {return}
+        let sourceViewController = segue.source as! ToDoDetailTableViewController
+        
+        if let toDo = sourceViewController.toDo {
+            if let indexOfExistingToDo = toDos.firstIndex(of: toDo) {
+                toDos[indexOfExistingToDo] = toDo
+                tableView.reloadRows(at: [IndexPath(row: indexOfExistingToDo, section: 0)], with: .automatic)
+            } else {
+            
+            
+            let newIndexPath = IndexPath(row: toDos.count, section: 0)
+            toDos.append(toDo)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+        }
+    }
+    
+    
+    
+  
+    
+   
+    
+}
